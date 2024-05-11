c1fb2d0b42daf4fbe559acf870395c8d756f03a9
nirbhay-design <sharma.59@iitj.ac.in>
2024-05-11 12:40:04 +0000

    verify loss function implementation

--- src/losses/LCAWSupCon.py
+++ src/losses/LCAWSupCon.py
@@ -5,6 +5,33 @@ import torch.nn.functional as F
 from src.dataset.tree import load_distances
 import pickle
 
+class SupConLoss(nn.Module):
+    def __init__(self, 
+                 sim = 'cosine', 
+                 tau = 1.0):
+        super().__init__()
+        self.tau = tau
+        self.sim = sim 
+
+    def forward(self, features, labels):
+        features = F.normalize(features, dim = -1, p = 2)
+        loss = 0
+        for idx, i in enumerate(features):
+            total_sim = 0
+            pos_samples_wt = []
+            num_pos = 0
+            for jdx, j in enumerate(features):
+                if idx != jdx:
+                    sim_ij = torch.exp(torch.sum(i*j) / self.tau)
+                    total_sim += sim_ij 
+                    if labels[idx] == labels[jdx]: # positive_sample
+                        pos_samples_wt.append(sim_ij)
+                        num_pos += 1
+
+            log_softmax = -torch.log(torch.tensor(pos_samples_wt) / total_sim).sum() / (num_pos + 1e-5)
+            loss += log_softmax
+        return loss / features.shape[0]            
+
 class LCAWSupConLoss(nn.Module):
     def __init__(self, 
                  hierarchy_dist_path, 
@@ -25,9 +52,9 @@ class LCAWSupConLoss(nn.Module):
         # calculate pair wise similarity 
         sim_mat = self.calculate_sim_matrix(features)
         # calculating lca weighted mask
-        lca_wt_mask = self.calculate_lca_weight_mask(labels)
+        # lca_wt_mask = self.calculate_lca_weight_mask(labels)
         # division by temperature
-        sim_mat = F.log_softmax(sim_mat * lca_wt_mask / self.tau, dim = -1) 
+        sim_mat = F.log_softmax(sim_mat / self.tau, dim = -1) 
         sim_mat = sim_mat.clone().fill_diagonal_(torch.tensor(0.0))
         # calculating pair wise equal labels for pos pairs
         labels = labels.unsqueeze(1)
@@ -76,91 +103,6 @@ class LCAConClsLoss(nn.Module):
     def forward(self, features, scores, labels):
         return self.lcasupcon(features, labels) + self.ce(scores, labels)
 
-class SupConLoss(nn.Module):
-    """Supervised Contrastive Learning: https://arxiv.org/pdf/2004.11362.pdf.
-    It also supports the unsupervised contrastive loss in SimCLR"""
-    def __init__(self, temperature=0.07, contrast_mode='all',
-                 base_temperature=0.07):
-        super(SupConLoss, self).__init__()
-        self.temperature = temperature
-        self.contrast_mode = contrast_mode
-        self.base_temperature = base_temperature
-
-    def forward(self, features, labels=None, mask=None):
-        device = (torch.device('cuda')
-                  if features.is_cuda
-                  else torch.device('cpu'))
-
-        if len(features.shape) < 3:
-            raise ValueError('`features` needs to be [bsz, n_views, ...],'
-                             'at least 3 dimensions are required')
-        if len(features.shape) > 3:
-            features = features.view(features.shape[0], features.shape[1], -1)
-
-        batch_size = features.shape[0]
-        if labels is not None and mask is not None:
-            raise ValueError('Cannot define both `labels` and `mask`')
-        elif labels is None and mask is None:
-            mask = torch.eye(batch_size, dtype=torch.float32).to(device)
-        elif labels is not None:
-            labels = labels.contiguous().view(-1, 1)
-            if labels.shape[0] != batch_size:
-                raise ValueError('Num of labels does not match num of features')
-            mask = torch.eq(labels, labels.T).float().to(device)
-        else:
-            mask = mask.float().to(device)
-
-        contrast_count = features.shape[1]
-        contrast_feature = torch.cat(torch.unbind(features, dim=1), dim=0)
-        if self.contrast_mode == 'one':
-            anchor_feature = features[:, 0]
-            anchor_count = 1
-        elif self.contrast_mode == 'all':
-            anchor_feature = contrast_feature
-            anchor_count = contrast_count
-        else:
-            raise ValueError('Unknown mode: {}'.format(self.contrast_mode))
-
-        # compute logits
-        anchor_dot_contrast = torch.div(
-            torch.matmul(anchor_feature, contrast_feature.T),
-            self.temperature)
-        # for numerical stability
-        logits_max, _ = torch.max(anchor_dot_contrast, dim=1, keepdim=True)
-        logits = anchor_dot_contrast - logits_max.detach()
-
-        # tile mask
-        mask = mask.repeat(anchor_count, contrast_count)
-        # mask-out self-contrast cases
-        logits_mask = torch.scatter(
-            torch.ones_like(mask),
-            1,
-            torch.arange(batch_size * anchor_count).view(-1, 1).to(device),
-            0
-        )
-        mask = mask * logits_mask
-
-        # compute log_prob
-        exp_logits = torch.exp(logits) * logits_mask
-        log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
-
-        # compute mean of log-likelihood over positive
-        # modified to handle edge cases when there is no positive pair
-        # for an anchor point. 
-        # Edge case e.g.:- 
-        # features of shape: [4,1,...]
-        # labels:            [0,1,1,2]
-        # loss before mean:  [nan, ..., ..., nan] 
-        mask_pos_pairs = mask.sum(1)
-        mask_pos_pairs = torch.where(mask_pos_pairs < 1e-6, 1, mask_pos_pairs)
-        mean_log_prob_pos = (mask * log_prob).sum(1) / mask_pos_pairs
-
-        # loss
-        loss = - (self.temperature / self.base_temperature) * mean_log_prob_pos
-        loss = loss.view(anchor_count, batch_size).mean()
-
-        return loss
-
 if __name__ == "__main__":
     x1 = torch.rand(4,5)
     x2 = torch.rand(4,5)
@@ -173,7 +115,5 @@ if __name__ == "__main__":
         "src/dataset/hierarchy_pkl", 
         "src/dataset/hierarchy_pkl/tieredimg_idx_to_cls.pkl",
         sim = 'mse')
-    supcon = SupConLoss(temperature = 1.0, base_temperature=1.0)
     print(lcswsupcon(x1, labels))
     print(lcssupcon_mse(x1, labels))
-    print(supcon(x1.unsqueeze(1), labels))
--- src/network/Network.py
+++ src/network/Network.py
@@ -2,28 +2,47 @@ import torch
 import torchvision 
 import torch.nn as nn 
 
-class Network(nn.Module):
-    def __init__(self, num_classes):
+class MLP(nn.Module):
+    def __init__(self, in_features, num_classes):
         super().__init__()
-        model = torchvision.models.resnet50(weights=torchvision.models.ResNet50_Weights.DEFAULT)
+        self.mlp = nn.Sequential(
+            nn.Linear(in_features, in_features),
+            nn.ReLU(),
+            nn.Linear(in_features, num_classes)
+        )
+
+    def forward(self, x):
+        return self.mlp(x)
+
+class Network(nn.Module):
+    def __init__(self, model_name = 'resnet18', pretrained = False):
+        super().__init__()
+        if model_name == 'resnet50':
+            model = torchvision.models.resnet50(
+                weights=torchvision.models.ResNet50_Weights.DEFAULT if pretrained else None)
+        elif model_name  == 'resnet18':
+            model = torchvision.models.resnet18(
+                weights=torchvision.models.ResNet18_Weights.DEFAULT if pretrained else None)
+        else:
+            print(f"{model_name} model type not supported")
+            model = None
         module_keys = list(model._modules.keys())
         self.feat_extractor = nn.Sequential()
         for key in module_keys[:-1]:
             module_key = model._modules.get(key, nn.Identity())
             self.feat_extractor.add_module(key, module_key)
-        resnet_classifier = model._modules.get(module_keys[-1], nn.Identity())
 
-        resnet_cls_infeature = resnet_classifier.in_features
-        self.classifier = nn.Linear(in_features = resnet_cls_infeature, out_features = num_classes)
+        self.classifier_infeatures = model._modules.get(module_keys[-1], nn.Identity()).in_features
 
     def forward(self, x):
         features = self.feat_extractor(x).flatten(1)
-        scores = self.classifier(features)
-        return features, scores
+        return features
 
 if __name__ == "__main__":
-    network = Network(num_classes=10)
-    x = torch.rand(2,3,84,84)
-    feat, score = network(x)
-    print(network)
-    print(feat.shape, score.shape)
+    network = Network(model_name = 'resnet50', pretrained=False)
+    mlp = MLP(network.classifier_infeatures, num_classes=10)
+    x = torch.rand(2,3,224,224)
+    feat = network(x)
+    print(feat.shape)
+    score = mlp(feat)
+    print(score.shape)
